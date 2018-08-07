fe971f3408b83c5e5f23c68e5ef7b56a6dc8354d
Feny Roy <FenyRoy@users.noreply.github.com>
2018-08-07 11:49:53 +0530

    Initial Stage

---
+++ app/src/main/java/com/trial/efcorp/ieeequizapp/MainActivity.java
@@ -0,0 +1,122 @@
+package com.trial.efcorp.ieeequizapp;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    String[] questions,choice1,choice2,choice3,choice4,answer;
+    String filename1, filename2, filename3, filename4,filename5,filename6;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        questions  = new String[] { "First man on moon?",
+                "First man on space?" };
+
+        choice1  = new String[] { "Edwin Aldrin",
+                "Laika" };
+
+        choice2  = new String[] { "Neil Armstrong",
+                "Valentina Tereshkova" };
+
+        choice3  = new String[] { "Aldrin Buzz",
+                "Yuri Gagarin" };
+
+        choice4  = new String[] { "Daniel Mcolin",
+                "Sputnik" };
+
+        answer  = new String[] { "Neil Armstrong",
+                "Yuri Gagarin" };
+
+        filename1="questions";
+        filename2="choice1";
+        filename3="choice2";
+        filename4="choice3";
+        filename5="choice4";
+        filename6="answer";
+
+        saveToFile();
+
+        Intent intent = new Intent(getApplicationContext(),QuizActivity.class);
+        startActivity(intent);
+    }
+
+    private void saveToFile() {
+        FileOutputStream fos1;
+        try {
+            fos1 = getApplicationContext().openFileOutput(filename1, Context.MODE_PRIVATE);
+            ObjectOutputStream oos = new ObjectOutputStream(fos1);
+            oos.writeObject(questions);
+            oos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        FileOutputStream fos2;
+        try {
+            fos2 = getApplicationContext().openFileOutput(filename2, Context.MODE_PRIVATE);
+            ObjectOutputStream oos = new ObjectOutputStream(fos2);
+            oos.writeObject(choice1);
+            oos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        FileOutputStream fos3;
+        try {
+            fos3 = getApplicationContext().openFileOutput(filename3, Context.MODE_PRIVATE);
+            ObjectOutputStream oos = new ObjectOutputStream(fos3);
+            oos.writeObject(choice2);
+            oos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        FileOutputStream fos4;
+        try {
+            fos4 = getApplicationContext().openFileOutput(filename4, Context.MODE_PRIVATE);
+            ObjectOutputStream oos = new ObjectOutputStream(fos4);
+            oos.writeObject(choice3);
+            oos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        FileOutputStream fos5;
+        try {
+            fos5 = getApplicationContext().openFileOutput(filename5, Context.MODE_PRIVATE);
+            ObjectOutputStream oos = new ObjectOutputStream(fos5);
+            oos.writeObject(choice4);
+            oos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        FileOutputStream fos6;
+        try {
+            fos6 = getApplicationContext().openFileOutput(filename6, Context.MODE_PRIVATE);
+            ObjectOutputStream oos = new ObjectOutputStream(fos6);
+            oos.writeObject(answer);
+            oos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+}
+
---
+++ app/src/main/java/com/trial/efcorp/ieeequizapp/QuizActivity.java
@@ -0,0 +1,222 @@
+package com.trial.efcorp.ieeequizapp;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.util.concurrent.TimeUnit;
+
+public class QuizActivity extends AppCompatActivity {
+
+    private TextView timeVal;
+    Handler customHandler = new Handler();
+    long startTime=0L,timeInMilliseconds=0L,timeSwapBuff=0L,updatetime=0L;
+    private TextView mQuestion;
+    private Button mChoice1,mChoice2,mChoice3,mChoice4;
+
+    Integer Score,i;
+
+    String[] questions,choice1,choice2,choice3,choice4,answer;
+    String filename1, filename2, filename3, filename4,filename5,filename6;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+
+        filename1="questions";
+        filename2="choice1";
+        filename3="choice2";
+        filename4="choice3";
+        filename5="choice4";
+        filename6="answer";
+
+        loadfromfile();
+
+        mQuestion = findViewById(R.id.questionField);
+        mChoice1 = findViewById(R.id.choice1);
+        mChoice2 = findViewById(R.id.choice2);
+        mChoice3 = findViewById(R.id.choice3);
+        mChoice4 = findViewById(R.id.choice4);
+
+        startTime = SystemClock.uptimeMillis();
+        timeVal = findViewById(R.id.projtime);
+        customHandler.postDelayed(updateTimeThread,0);
+
+        i=0;
+        updatequiz();
+
+    }
+
+    Runnable updateTimeThread = new Runnable() {
+        @Override
+        public void run() {
+            timeInMilliseconds = SystemClock.uptimeMillis()-startTime;
+            updatetime= timeSwapBuff+timeInMilliseconds;
+            int secs = (int)(updatetime/1000);
+            int mins = secs/60;
+            secs%=60;
+            int milliseconds= (int)updatetime%1000;
+            timeVal.setText(""+mins+":"+String.format("%2d",secs)+":"+String.format("%3d",milliseconds));
+            customHandler.postDelayed(this,0);
+        }
+    };
+
+    private void updatequiz(){
+
+        if(i>1){
+
+            Intent resultIntent = new Intent(getApplicationContext(),ResultActivity.class);
+            startActivity(resultIntent);
+        }
+
+            mQuestion.setText(questions[i]);
+            mChoice1.setText(choice1[i]);
+            mChoice2.setText(choice2[i]);
+            mChoice3.setText(choice3[i]);
+            mChoice4.setText(choice4[i]);
+
+            mChoice1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                   if(mChoice1.getText()==answer[i]) {
+                       Score += 1;
+                   }
+                    i+=1;
+                    updatequiz();
+                }
+            });
+
+
+            mChoice2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    if(mChoice2.getText()==answer[i]) {
+                        Score += 1;
+                    }
+
+                    i+=1;
+                    updatequiz();
+
+                }
+            });
+
+            mChoice3.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    if(mChoice3.getText()==answer[i]) {
+                        Score += 1;
+
+                    }
+                    i+=1;
+                    updatequiz();
+
+                }
+            });
+
+            mChoice4.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    if(mChoice4.getText()==answer[i]) {
+                        Score += 1;
+
+                    }
+
+                    i+=1;
+                    updatequiz();
+                }
+            });
+
+
+    }
+
+    private void loadfromfile() {
+
+        FileInputStream fis1;
+        try
+        {
+            fis1 = openFileInput(filename1);
+            ObjectInputStream ois = new ObjectInputStream(fis1);
+            questions =(String[]) ois.readObject();
+            ois.close();
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+
+        FileInputStream fis2;
+        try
+        {
+            fis2 = openFileInput(filename2);
+            ObjectInputStream ois = new ObjectInputStream(fis2);
+            choice1 =(String[]) ois.readObject();
+            ois.close();
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        FileInputStream fis3;
+        try
+        {
+            fis3 = openFileInput(filename3);
+            ObjectInputStream ois = new ObjectInputStream(fis3);
+            choice2 =(String[]) ois.readObject();
+            ois.close();
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        FileInputStream fis4;
+        try
+        {
+            fis4 = openFileInput(filename4);
+            ObjectInputStream ois = new ObjectInputStream(fis4);
+            choice3 =(String[]) ois.readObject();
+            ois.close();
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        FileInputStream fis5;
+        try
+        {
+            fis5 = openFileInput(filename5);
+            ObjectInputStream ois = new ObjectInputStream(fis5);
+            choice4 =(String[]) ois.readObject();
+            ois.close();
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        FileInputStream fis6;
+        try
+        {
+            fis6 = openFileInput(filename6);
+            ObjectInputStream ois = new ObjectInputStream(fis6);
+            answer =(String[]) ois.readObject();
+            ois.close();
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+
+    }
+
+}
